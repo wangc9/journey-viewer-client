83b3b2a182f330e00b0d25e84176679ff2d3d8a9
Chen Wang <wang.756090@gmail.com>
2025-05-06 14:39:17 +0000

    Chore: Add posthog

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,16 +4,19 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { getQueryClient } from "./getQueryClient";
 import { ReactNode } from "react";
 import { MapProvider } from "@/context/MapContext";
+import { PostHogProvider } from "./PostHogProvider";
 
 export default function Providers({ children }: { children: ReactNode }) {
   const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <MapProvider>
-        {children}
-        <ReactQueryDevtools />
-      </MapProvider>
-    </QueryClientProvider>
+    <PostHogProvider>
+      <QueryClientProvider client={queryClient}>
+        <MapProvider>
+          {children}
+          <ReactQueryDevtools />
+        </MapProvider>
+      </QueryClientProvider>
+    </PostHogProvider>
   );
 }
