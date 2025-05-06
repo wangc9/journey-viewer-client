93da984b569c6d13dc38e79df676d3032e59995a
Chen Wang <wang.756090@gmail.com>
2025-05-06 15:40:33 +0000

    Feat: Wrap popular destination list in accordion

--- src/components/StationCard.tsx
+++ src/components/StationCard.tsx
@@ -6,6 +6,12 @@ import { useSuspenseQueries } from "@tanstack/react-query";
 import NumberCard from "./NumberCard";
 import { useTranslations } from "next-intl";
 import { Table, TableBody, TableCell, TableRow } from "./ui/table";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./ui/accordion";
 
 export default function StationCard({ stationId }: StationCardProps) {
   const t = useTranslations("StationCard");
@@ -42,41 +48,52 @@ export default function StationCard({ stationId }: StationCardProps) {
         </section>
         {destinationQuery.data !== undefined &&
           !("error" in destinationQuery.data) && (
-            <section className="px-4">
-              <Table>
-                <TableBody>
-                  {destinationQuery.data.map((station) => (
-                    <TableRow key={station.id}>
-                      <TableCell
-                        className="grid cursor-pointer grid-cols-2"
-                        // onClick={() => {
-                        //   setCoordinate(() => {
-                        //     return {
-                        //       lat: station.coordinateY ?? null,
-                        //       lng: station.coordinateX ?? null,
-                        //     };
-                        //   });
-                        //   setSelectedStation(station);
-                        //   triggerRef.current?.click();
-                        // }}
-                      >
-                        <article className="flex flex-col gap-y-1">
-                          <span className="text-lg font-medium">
-                            {station.station_name ?? ""}
-                          </span>
-                          <span className="font-light text-gray-600">
-                            {station.station_address ?? ""}
-                          </span>
-                        </article>
-                        <span className="self-center text-center text-lg font-semibold">
-                          {station.journey_count ?? ""}
-                        </span>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </section>
+            <Accordion type="single" collapsible>
+              <AccordionItem value="popularDestinations" className="px-4">
+                <AccordionTrigger>
+                  <span className="font-semibold">
+                    {t("popular_destinations")}
+                  </span>
+                </AccordionTrigger>
+                <AccordionContent>
+                  <section className="px-4">
+                    <Table>
+                      <TableBody>
+                        {destinationQuery.data.map((station) => (
+                          <TableRow key={station.id}>
+                            <TableCell
+                              className="grid cursor-pointer grid-cols-2"
+                              // onClick={() => {
+                              //   setCoordinate(() => {
+                              //     return {
+                              //       lat: station.coordinateY ?? null,
+                              //       lng: station.coordinateX ?? null,
+                              //     };
+                              //   });
+                              //   setSelectedStation(station);
+                              //   triggerRef.current?.click();
+                              // }}
+                            >
+                              <article className="flex flex-col gap-y-1">
+                                <span className="text-lg font-medium">
+                                  {station.station_name ?? ""}
+                                </span>
+                                <span className="font-light text-gray-600">
+                                  {station.station_address ?? ""}
+                                </span>
+                              </article>
+                              <span className="self-center text-center text-lg font-semibold">
+                                {station.journey_count ?? ""}
+                              </span>
+                            </TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </section>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           )}
       </section>
     );
