01b47377041eefbaf571ddeb28ceed85daf7b7bf
Chen Wang <wang.756090@gmail.com>
2025-05-07 17:36:51 +0000

    Fix: Update map setting

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -41,7 +41,7 @@ export default function Map({
       >
         <TileLayer
           attribution='&copy; <a href="https://www.digitransit.fi/en/developers/apis/6-terms-of-use/">Digitransit</a> contributors'
-          url="https://cdn.digitransit.fi/map/v3/hsl-map/{z}/{x}/{y}@2x.png?digitransit-subscription-key=e272a3da0ede40f0bfe2b95083b33298"
+          url={`https://cdn.digitransit.fi/map/v3/hsl-map/{z}/{x}/{y}@2x.png?digitransit-subscription-key=${process.env.NEXT_PUBLIC_DIGITRANSIT_KEY}`}
         />
         <MapEvents
           setIcon={setIcon}
