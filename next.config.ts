2b3cede8af1ea727e0ba85ac562ae96614b59014
Chen Wang <wang.756090@gmail.com>
2025-05-09 09:42:26 +0000

    Fix: Change CORS

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  crossOrigin: "use-credentials",
 };
 
 const withNextIntl = createNextIntlPlugin();
